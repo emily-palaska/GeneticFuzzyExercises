39f1cfb6bf1a5d9807440aea9a2218c353028774
Emily Palaska <aimilia.p2@gmail.com>
2025-05-22 12:21:07 +0300

    results for 9_1 and new plot function with errorbar

--- genetic/9_1.py
+++ genetic/9_1.py
@@ -1,21 +1,21 @@
 import numpy as np
 from genetic_algorithm import GeneticAlgorithm, linear_fitness
-from plot_functions import p_delta_histograms
-
+from plot_functions import p_delta_histograms, p_delta_line
 def main():
-    runs, p_deltas = 20, [0.7, 0.0]
-    P_mu, n, l = 0.001, 1000, 100
+    runs, p_deltas = 20, [0.1*i for i in range(11)]
+    P_mu, n, l = 0.001, 100, 20
     results = []
 
     for P_delta in p_deltas:
         ga = GeneticAlgorithm(P_delta=P_delta, fitness=linear_fitness, P_mu=P_mu, n=n, l=l)
         generations = []
         for r in range(runs):
-            generations.append(ga.run())
+            generations.append(ga.run(max_generations=1e4))
             print(f'\tRun {r}: {generations[-1]}')
         results.append(generations)
         print(f"Average generation for P_delta = {P_delta}: {np.mean(generations):.2f}")
-    p_delta_histograms(results, p_deltas, '../results/9_1_hist.png')
+    #p_delta_histograms(results, p_deltas, '../results/9_1_hist.png')
+    p_delta_line(results, p_deltas, '../results/9_1_line.png')
 
 if __name__ == "__main__":
     main()
--- genetic/plot_functions.py
+++ genetic/plot_functions.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def p_delta_histograms(results: list, p_deltas: list, path: str):
-    colors = ['blue', 'orange']
+    colors = ['blue', 'orange', 'green', 'red', 'purple']
     plt.figure(figsize=(10, 6))
 
     for i, (gen_data, p_delta) in enumerate(zip(results, p_deltas)):
@@ -18,6 +18,23 @@ def p_delta_histograms(results: list, p_deltas: list, path: str):
     plt.grid(True)
     plt.tight_layout()
     plt.savefig(path)
+
+def p_delta_line(results: list, p_deltas: list, path: str):
+    results = np.array(results)
+    p_deltas = np.array(p_deltas)
+
+    mean_vals = np.mean(results, axis=1)
+    std_vals = np.std(results, axis=1)
+
+    plt.figure(figsize=(10, 6))
+    plt.errorbar(p_deltas, mean_vals, yerr=std_vals,color='purple', fmt='-o', capsize=5)
+    plt.xlabel("P_delta Values")
+    plt.ylabel("Average Generations to Reach Target")
+    plt.title("Average Convergence Generations for Different Crossover Probabilities")
+    plt.grid(True)
+    plt.tight_layout()
+    plt.savefig(path)
+
 
 def generation_progress(generations, bests, means, path: str):
     plt.figure(figsize=(10, 6))
